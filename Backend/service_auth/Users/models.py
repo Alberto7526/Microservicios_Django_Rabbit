c40c3718b1da9b42248c5a3179a95cf3d57941c6
RoboticaSoftware <desarrollo.software@robotica.com.co>
2025-01-03 16:27:27 -0500

    modified user model

--- Backend/service_auth/Users/models.py
+++ Backend/service_auth/Users/models.py
@@ -2,12 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User (AbstractUser):
-    user_login = models.CharField('Usuario', max_length = 255)
     user_name = models.CharField('Nombre', max_length = 255)
     user_last_name = models.CharField('Apellidos', max_length=255)
-    user_password = models.BinaryField('Contraseña', max_length= 255)
     user_phone = models.CharField('Telefono', max_length=50, blank= True, null=True)
-    user_email = models.EmailField('Correo Electrónico', max_length=255, unique=True)
     user_address = models.CharField('Dirección', max_length=255, blank=True, null=True)
     user_enable = models.BooleanField('Activo', default=True)
     user_lock = models.BooleanField('Bloqueado', default=False)
