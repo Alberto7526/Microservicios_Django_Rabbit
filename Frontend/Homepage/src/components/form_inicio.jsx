74a415cbcc298a858759731494e6344537c33c9c
devsan-g3 <desarrollo.backend@robotica.com.co>
2025-01-09 15:31:14 -0500

    DarkMode Finish

--- Frontend/Homepage/src/components/form_inicio.jsx
+++ Frontend/Homepage/src/components/form_inicio.jsx
@@ -1,54 +1,54 @@
 const Form_init = () => {
   return (
     <div className="dark">
-      <div className="flex items-center justify-center min-h-screen bg-[#edeef1] dark:bg-[#1a202c] border border-[#d7dbe0] dark:border-[#4a5568]">
-        <div className="relative flex flex-col m-6 space-y-8 bg-white dark:bg-slate-800 shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+      <div className="flex items-center justify-center min-h-screen bg-[#edeef1] dark:bg-[#6d6d6d] border border-[#d7dbe0] dark:border-[#4a5568]">
+        <div className="relative flex flex-col m-6 space-y-8 bg-white dark:bg-[#1f1f1f] shadow-2xl rounded-2xl md:flex-row md:space-y-0">
           <div className="flex flex-col justify-center p-2 md:p-11">
             <span className="mb-3 text-4xl text-[#8C1919] dark:text-[#ffffff] text-center font-bold">
               Bienvenido
             </span>
-            <span className="text-[#44403C] dark:text-gray-300 mb-3 font-semibold text-center">
+            <span className="text-[#44403C] dark:text-[#878787] mb-3 font-semibold text-center">
               Por favor ingresar tu correo
             </span>
             <div className="py-4">
-              <span className="mb-2 text-md text-[#8C1919] dark:text-[#ffffff] font-semibold">
+              <span className="mb-2 text-md text-[#8C1919] dark:text-[#878787] font-semibold">
                 Email
               </span>
               <input
                 type="text"
-                className="w-full p-2 border border-gray-300 dark:border-gray-600 hover:border-red-700 focus:border-red-700 dark:bg-slate-900 dark:text-white rounded-md placeholder:font-light placeholder:text-red-700"
+                className="w-full p-2 border border-gray-300 dark:border-[#626362] hover:border-red-700 focus:border-red-700 dark:bg-[#242424] dark:text-white dark:hover:border-[#51814f] rounded-md placeholder:font-light placeholder:text-red-700"
                 name="email"
                 id="email"
               />
             </div>
             <div className="py-2">
-              <span className="mb-2 text-md text-[#8C1919] dark:text-[#ffffff] font-semibold">
+              <span className="mb-2 text-md text-[#8C1919] dark:text-[#878787] font-semibold">
                 Contraseña
               </span>
               <input
                 type="password"
                 name="pass"
                 id="pass"
-                className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-md placeholder:font-light hover:border-red-700 focus:border-red-700 dark:bg-slate-900 dark:text-white placeholder:text-gray-500"
+                className="w-full p-2 border border-gray-300 dark:border-[#626362] rounded-md placeholder:font-light hover:border-red-700 focus:border-red-700 dark:bg-[#242424] dark:text-white dark:hover:border-[#51814f] placeholder:text-gray-500"
               />
             </div>
-            <div className="flex justify-between w-full py-4 text-[#44403C] dark:text-gray-300">
-              <div className="mr-24 accent-[#8C1919] dark:accent-[#e53e3e]">
+            <div className="flex justify-between w-full py-4 text-[#44403C] dark:text-[#878787]">
+              <div className="mr-24 accent-[#8C1919] dark:accent-[#51814f]">
                 <input type="checkbox" name="ch" id="ch" className="mr-2" />
                 <span className="text-md">Recordar por 30 días</span>
               </div>
-              <span className="font-bold text-md text-[#8C1919] dark:text-[#ffffff]">
+              <span className="font-bold text-md text-[#8C1919] dark:text-[#878787]">
                 Olvidé mi contraseña
               </span>
             </div>
-            <button className="w-full bg-[#8C1919] text-white p-2 rounded-lg mb-6 hover:bg-[#480707] hover:bg-opacity-90 hover:text-white hover:border hover:border-gray-300 font-semibold dark:bg-[#e53e3e] dark:hover:bg-[#9b2c2c]">
+            <button className="w-full bg-[#8C1919] text-white p-2 rounded-lg mb-6 hover:bg-[#480707] hover:bg-opacity-90 hover:text-white hover:border hover:border-gray-300 font-semibold dark:bg-[#626362] dark:hover:bg-[#454545]">
               Iniciar Sesión
             </button>
             <div className="text-center text-gray-400 dark:text-gray-500">
-              <span className="font-semibold text-[#44403C] dark:text-[#e53e3e]">Ó</span>
+              <span className="font-semibold text-[#44403C] dark:text-[#878787]">Ó</span>
             </div>
             <div className="py-2"></div>
-            <button className="w-full border border-gray-300 text-md p-2 rounded-lg mb-6 hover:bg-[#8C1919] hover:bg-opacity-90 hover:text-white dark:bg-slate-700 dark:hover:bg-[#e53e3e]">
+            <button className="w-full border border-gray-300 text-md p-2 rounded-lg mb-6 hover:bg-[#8C1919] hover:bg-opacity-90 hover:text-white dark:bg-[#242424] dark:hover:bg-[#454545] dark:text-white">
               <img
                 src="Microsoft_logo.svg"
                 alt="Microsoft Icon"
